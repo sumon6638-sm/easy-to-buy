51fd3e306092387817680ce2d63990b4c4a49fc2
SuMon6638 <confuce.sumon.78656842@gmail.com>
2021-09-12 22:24:56 +0600

    Ready_check

---
+++ README.md
@@ -0,0 +1,36 @@
+## Repository link: https://github.com/Programming-Hero-Web-Course4/ranga-store-fix-sumon6638-sm
+
+## Live site link:
+
+## I have to do:
+
+1. Prothom dik theke 3ta kore row thakbe...sese 2-1ta thakte pare... --> okk
+
+2. Pic gula dekhate hbe... --> okk
+
+3. Total Added-Products: theke $ (dollar) k remove korte hbe... --> okk
+
+4. Total k update korte hbe... --> okk
+
+5. Fractional number gula perfect vabe add hcce na...seta k thik korte hbe... 2 decimal... --> okk
+
+6. Buy Now is optional... as your wish...
+
+7. kiso likhe search dile dynamically product gula update hbe... optional...
+
+8. Bonus Marks:
+
+   1. Rating related information 2ta --> okk
+
+      1. kotojon rating dise...
+      2. R gor rating koto?
+
+   2. Search button dekhte sundor na...seta k dekhte sundor korte hbe... --> okk
+
+   3. Website er name change korte hbe... --> okk
+
+   4. Card er color change krte hbe... reasonable...
+
+   5. Details ee click korle details information dekhate hbe...search button er nice dekhate hbe or modal diye jkno vabe dekhate hbe...API theke load kore...(alert, confirm, prompt use kora jabe na...) --> okk
+
+   6. code organize korte hbe...
--- js/app.js
+++ js/app.js
@@ -13,15 +13,64 @@ const showProducts = (products) => {
     const image = product.images;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+    div.innerHTML = `<div class="col">
+            <div class="card border-success border-2 h-100">
+
+              <!-- Start Card Thumbnail -->
+              <div>
+                <img class="product-image image-fluid" src=${product.image}></img>
+              </div>
+
+              <!-- Start Card Details or Card BODY -->
+              <div class="card-body">
+                <h4>${product.title}</h4>
+                <p><b>Category:</b> ${product.category}</p>
+                <h2>Price: $ ${product.price}</h2>
+              </div>
+
+              <!-- Start Card Footer Styling -->
+              <div class="card-footer" style="background-color: gray;">
+                <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn"
+                  class="buy-now btn btn-success">Add
+                  to cart</button>
+
+                <!-- popup button starting -->
+                <!-- Button trigger modal -->
+                <button type="button" id="details-btn" class="btn btn-danger my-button" data-bs-toggle="modal"
+                  data-bs-target="#staticBackdrop">
+                  Details
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+                  tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title text-dark" id="staticBackdropLabel">${product.title}
+                        </h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        <p class="text-dark my-text"><strong>About this product:</strong> ${product.description}</p>
+                        <p class="text-dark my-text"><strong>Category:</strong> ${product.category}</p>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- popup button end -->
+              </div>
+
+              <div class='text-white' style="background-color: gray;">
+                <P>Approximately <b>${product.rating.count}</b> person rate this product.</p>
+                <p>It's average product rate is: <b>${product.rating.rate}</b></p>
+              </div>
+              <!--End Card FOOTER Styling -->
+            </div>
+          </div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
@@ -32,6 +81,7 @@ const addToCart = (id, price) => {
   updatePrice("price", price);
 
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
@@ -46,12 +96,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = parseFloat(total).toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = parseFloat(value).toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -73,8 +123,6 @@ const updateTaxAndCharge = () => {
 
 //grandTotal update function
 const updateTotal = () => {
-  const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
-    getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  const grandTotal = getInputValue("price") + getInputValue("delivery-charge") + getInputValue("total-tax");
+  document.getElementById("total").innerText = parseFloat(grandTotal).toFixed(2);
 };
